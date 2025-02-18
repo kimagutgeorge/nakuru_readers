87fbd3532c48b09ede269488664e5ab72d3ad31a
George Kimagut <geojimagut@gmail.com>
2025-02-18 17:57:01 +0300

    <home page/ui>

--- readers_users/src/router/index.js
+++ readers_users/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UserSignUp from '@/views/UserSignUp.vue'
 import UserLogin from '@/views/UserLogin.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/signup',
     name: 'Sign Up',
     component: UserSignUp
+  },
+  {
+    path:'/home',
+    name: 'HomeView',
+    component: HomeView
   }
 ]
 
--- readers_users/src/views/HomeView.vue
+++ readers_users/src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-wrapper">
+    <UserResponse v-if="responseClass.includes('displayed')" :class="['response-message', responseClass]" :dbResponse="dbResponse" @close="closeResponse" />
+    <div class="home-wrapper col-100">
+      <div class="home-top-bar">
+        <div class="col-100 col-flex">
+          <div class="col-80 col-flex">
+            <div class="col-10">
+              <img src="../assets/860.jpg" class="home-logo">
+            </div>
+            <div class="col-90">
+              <p>Welcome, <br>
+              <span class="fw-bold">George</span></p>
+            </div>
+          </div>
+          <div class="col-20">
+            <i class="fa-solid fa-bell"></i>
+          </div>
+        </div>
+      </div>
+      <div class="home-main-bar col-flex">
+        <div class="col-30 text-center">
+          <i class="fa-solid fa-envelope"></i>
+        </div>
+        <div class="col-30 text-center">
+          <i class="fa-solid fa-shopping-cart"></i>
+        </div>
+        <div class="col-30 text-center">
+          <i class="fa-solid fa-book-open-reader"></i>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import UserResponse from '@/components/UserResponse.vue';
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { UserResponse },
+  data() {
+      return {
+        responseClass: '',
+        dbResponse: '',
+        email: '',
+        password: '',
+      }
+    },
+    methods: {
+      closeResponse() {
+        this.responseClass = '';
+        this.dbResponse = '';
+      },
+    }
 }
 </script>
