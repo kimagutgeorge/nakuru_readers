b055faa11d280b374482ad9734fdf145ca5e6770
George Kimagut <geojimagut@gmail.com>
2025-02-19 10:01:01 +0300

    <complete home UI>

---
+++ readers_users/src/components/UserNavigation.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="nav-bar col-flex col-100">
+        <div class="col-20 text-center">
+            <router-link to="/home" class="col-100 color-dark" active-class="is-active">
+                <i class="fa-solid fa-home"></i>
+            </router-link>
+        </div>
+        <div class="col-20 text-center">
+            <router-link to="/reads" class="col-100 color-dark" active-class="is-active">
+                <i class="fa-solid fa-book-open-reader"></i>
+            </router-link>
+        </div>
+        <div class="col-20 text-center">
+            <router-link to="/shop" class="col-100 color-dark" active-class="is-active">
+                <i class="fa-solid fa-shopping-cart"></i>
+            </router-link>
+        </div>
+        <div class="col-20 text-center">
+            <router-link to="/events" class="col-100 color-dark" active-class="is-active">
+                <i class="fa-solid fa-calendar"></i>
+            </router-link>
+        </div>
+        <div class="col-20 text-center">
+            <router-link to="/messages" class="col-100 color-dark" active-class="is-active">
+                <i class="fa-solid fa-envelope"></i>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserNavigation',
+
+}
+</script>
--- readers_users/src/views/HomeView.vue
+++ readers_users/src/views/HomeView.vue
@@ -13,32 +13,97 @@
               <span class="fw-bold">George</span></p>
             </div>
           </div>
-          <div class="col-20">
+          <div class="col-20 col-flex">
             <i class="fa-solid fa-bell"></i>
+            <i class="fa-solid fa-power-off"></i>
           </div>
         </div>
       </div>
       <div class="home-main-bar col-flex">
-        <div class="col-30 text-center">
+        <div class="col-33 text-center">
           <i class="fa-solid fa-envelope"></i>
         </div>
-        <div class="col-30 text-center">
+        <div class="col-33 text-center">
           <i class="fa-solid fa-shopping-cart"></i>
         </div>
-        <div class="col-30 text-center">
+        <div class="col-33 text-center">
           <i class="fa-solid fa-book-open-reader"></i>
         </div>
       </div>
+      <!-- quick actions -->
+      <div class="home-main-actions col-flex">
+        <div class="col-100">
+          <p>Quick Actions</p>
+        </div>
+        <div class="col-33 text-center">
+          <div class="col-40 text-center">
+            <i class="fa-solid fa-list"></i>
+            <p>Books</p>
+          </div>
+        </div>
+        <div class="col-33 text-center">
+          <div class="col-40 text-center">
+            <i class="fa-solid fa-star text-center"></i>
+            <p>Favourites</p>
+          </div>
+        </div>
+        <div class="col-33 text-center">
+          <div class="col-40 text-center">
+            <i class="fa-solid fa-calendar text-center"></i>
+            <p>Events</p>
+          </div>
+        </div>
+        <div class="col-33 text-center">
+          <div class="col-40 text-center">
+            <i class="fa-solid fa-envelope text-center"></i>
+            <p>Messages</p>
+          </div>
+        </div>
+        <div class="col-33 text-center">
+          <div class="col-40 text-center">
+            <i class="fa-solid fa-store text-center"></i>
+            <p>Store</p>
+          </div>
+        </div>
+        <div class="col-33 text-center">
+          <div class="col-40 text-center">
+            <i class="fa-solid fa-list text-center"></i>
+            <p>Orders</p>
+          </div>
+        </div>
+      </div>
+      <!-- end of quick actions -->
+      <div class="home-main-actions col-flex">
+        <div class="col-100">
+          <p>Latest Messages</p>
+        </div>
+        <div class="col-100 msg-box">
+          <p class="fw-bold">Mtu Fulani</p>
+          <p>Hii ndo message ametuma</p>
+          <span class="fw-bold">02/08 20:46</span>
+        </div>
+        <div class="col-100 msg-box">
+          <p class="fw-bold">Mtu Fulani</p>
+          <p>Hii ndo message ametuma</p>
+          <span class="fw-bold">02/08 20:46</span>
+        </div>
+        <div class="col-100 text-center">
+          <span class="fw-bold default-color">See More</span>
+        </div>
+      </div>
     </div>
+    <!-- end of message -->
+     <UserNavigation/>
   </div>
 </template>
 
 <script>
 import UserResponse from '@/components/UserResponse.vue';
+import UserNavigation from '@/components/UserNavigation.vue';
 
 export default {
   name: 'HomeView',
-  components: { UserResponse },
+  components: { UserResponse, UserNavigation },
   data() {
       return {
         responseClass: '',
